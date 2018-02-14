a7a9a940f92bdd623aab1137485ce762428eb876
Andrew Woods <andrew_woods@rapid7.com>
2018-02-14 23:17:15 +0000

    Adding View

---
+++ app/src/main/java/com/rapid7/awoods/mission_to_mars/GameView.java
@@ -0,0 +1,99 @@
+package com.rapid7.awoods.mission_to_mars;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by awoods on 14/02/18.
+ */
+
+public class GameView extends SurfaceView implements Runnable{
+
+    volatile boolean playing;
+    private Thread gameThread = null;
+    //private Player player;
+
+    private Paint paint;
+    private Canvas canvas;
+    private SurfaceHolder surfaceHolder;
+    Bitmap bitmap;
+
+    public GameView(Context context, int screenX, int screenY) {
+        super(context);
+        //player = new Player(context, screenX, screenY);
+        //bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_background);
+        //canvas.drawBitmap(bitmap, 50, 50, paint);
+        surfaceHolder = getHolder();
+        paint = new Paint();
+    }
+
+    @Override
+    public void run() {
+        while (playing) {
+            update();
+            draw();
+            control();
+        }
+    }
+
+    private void update() {
+        //player.update();
+    }
+
+    private void draw() {
+        if (surfaceHolder.getSurface().isValid()) {
+            canvas = surfaceHolder.lockCanvas();
+            canvas.drawColor(Color.BLACK);
+
+            //canvas.drawBitmap(
+                    //player.getBitmap(),
+                    //player.getX(),
+                    //player.getY(),
+                    //paint);
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pause() {
+        playing = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void resume() {
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                //player.stopBoosting();
+                break;
+            case MotionEvent.ACTION_DOWN:
+                //player.setBoosting();
+                break;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/rapid7/awoods/mission_to_mars/Mission_To_Mars.java
+++ app/src/main/java/com/rapid7/awoods/mission_to_mars/Mission_To_Mars.java
@@ -1,13 +1,38 @@
 package com.rapid7.awoods.mission_to_mars;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class Mission_To_Mars extends Activity {
+public class Mission_To_Mars extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //set the view
         setContentView(R.layout.activity_mission__to__mars);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        GameView gameView = new GameView(this, size.x, size.y);
+
+        setContentView(gameView);
     }
 }
