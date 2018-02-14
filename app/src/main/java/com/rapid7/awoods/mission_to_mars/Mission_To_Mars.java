24f7da3c2d2aa8a9e688107423df6da082439f56
Andrew Woods <andrew_woods@rapid7.com>
2018-02-14 19:27:51 +0000

    Initial code

---
+++ app/src/main/java/com/rapid7/awoods/mission_to_mars/Mission_To_Mars.java
@@ -0,0 +1,13 @@
+package com.rapid7.awoods.mission_to_mars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mission_To_Mars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission__to__mars);
+    }
+}
